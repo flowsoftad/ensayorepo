dc3baa813cba25159d8ef36a724eb46d3a535b08
Diego <Diego@192.168.1.72>
2019-07-21 18:16:29 -0500

    ensayo

---
+++ presentacion/src/main/java/co/panaderia/web/PresentacionApplication.java
@@ -0,0 +1,13 @@
+package co.panaderia.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentacionApplication.class, args);
+	}
+
+}
---
+++ presentacion/src/test/java/co/panaderia/web/PresentacionApplicationTests.java
@@ -0,0 +1,16 @@
+package co.panaderia.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PresentacionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
